ffdccde6de5279d8329869a2f03da92928f33453
Dane Carmichael <carmichaeldane@gmail.com>
2016-11-16 08:33:01 -0600

    Creating a module in spec/contectio to hold fake responses.

--- spec/contextio/accounts_spec.rb
+++ spec/contextio/accounts_spec.rb
@@ -1,43 +1,7 @@
 require "contextio/accounts"
+require_relative "./mock_response.rb"
 
-RESPONSE = [{"created"=>1475765868,
-    "username"=>"some_accoount.gmail.com_some_numbers",
-    "suspended"=>nil,
-    "id"=>"some_numbers",
-    "email_addresses"=>["some_accoount.gmail.com"],
-    "first_name"=>"Some",
-    "last_name"=>"Account",
-    "password_expired"=>0,
-    "sources"=>
-     [{"server"=>"imap.googlemail.com",
-       "label"=>"some_account::gmail",
-       "username"=>"some_account.com",
-       "port"=>993,
-       "authentication_type"=>"oauth2",
-       "use_ssl"=>true,
-       "status"=>"OK",
-       "type"=>"imap",
-       "resource_url"=>"https://api.context.io/2.0/accounts/"}],
-    "resource_url"=>"https://api.context.io/2.0/accounts/"},
-   {"created"=>1479162410,
-    "username"=>"asecondaccount.gmail.com_some_numbers",
-    "suspended"=>nil,
-    "id"=>"some_other_numbers",
-    "email_addresses"=>["asecondaccount@gmail.com"],
-    "first_name"=>"Amuro",
-    "last_name"=>"Ray",
-    "password_expired"=>0,
-    "sources"=>
-     [{"server"=>"imap.googlemail.com",
-       "label"=>"asecondaccount::gmail",
-       "username"=>"qsecondaccount@gmail.com",
-       "port"=>993,
-       "authentication_type"=>"oauth2",
-       "use_ssl"=>true,
-       "status"=>"OK",
-       "type"=>"imap",
-       "resource_url"=>"https://api.context.io/2.0/accounts/"}],
-    "resource_url"=>"https://api.context.io/2.0/accounts/"}].freeze
+RESPONSE = MockResponse::ACCOUNTS
 
 describe Accounts do
   describe "An Accounts object holding more than one account" do
