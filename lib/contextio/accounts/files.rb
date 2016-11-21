d0e066a40083533c7a7fe3243603d61e1703807c
Dane Carmichael <carmichaeldane@gmail.com>
2016-11-21 10:11:04 -0600

    Fixing some lines of code I inadvertantly changed in Files.rb

--- lib/contextio/accounts/files.rb
+++ lib/contextio/accounts/files.rb
@@ -14,7 +14,7 @@ class Files
 
   def self.fetch(connection, account_id, email, method)
     raw_response = connection.connect.send(method,
-                                           "/2.0/accounts/#{account_id}/contacts/#{email}/files")
+                                           "/2.0/accounts/#{account_id}/files/")
     status = raw_response.status.to_s
     raw_response_body = raw_response.body
     parsed_response_body = JSON.parse(raw_response_body)
