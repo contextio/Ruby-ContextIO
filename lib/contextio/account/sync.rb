ad31d70f97f48b934252562954fc2f4300127ebe
Dane Carmichael <carmichaeldane@gmail.com>
2017-01-17 10:41:36 -0600

    Converting Sync to new object format, adding get and call_url methods

--- lib/contextio/account/sync.rb
+++ lib/contextio/account/sync.rb
@@ -1,10 +1,31 @@
 module ContextIO
   class Sync < BaseClass
-    attr_reader :response, :status, :success
-    def initialize(request)
+    private
+    attr_reader :parent
+
+    public
+    attr_reader :status, :success, :connection, :response
+    def initialize(parent:,
+                   response: nil,
+                   status: nil,
+                   success: nil)
+      @parent = parent
+      @connection = parent.connection
+      @response = response
+      @status = status
+      @success = success
+    end
+
+    def call_url
+      "#{parent.call_url}/sync"
+    end
+
+    def get
+      request = Request.new(connection, :get, call_url)
       @response = request.response
       @status = request.status
-      @success =  request.success
+      @success = check_success(status)
+      self
     end
   end
 end
