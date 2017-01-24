8719a181a1dc20c2d3512871267dfb7299b61252
Dane Carmichael <carmichaeldane@gmail.com>
2017-01-24 14:33:28 -0600

    Removing extra data from mock_response.rb

--- spec/contextio/utilities/mock_response.rb
+++ spec/contextio/utilities/mock_response.rb
@@ -74,46 +74,5 @@ module MockResponse
     \"authentication_type\":\"oauth2\",\"use_ssl\":true,\"status\":\"OK\",\"sync_flags\":false,
     \"type\":\"imap\",\"resource_url\":\"https:\\/\\/api.context.io\\/2.0\\/accounts\\/1234\\/sources\\/someemail%3A%3Agmail\"}]}".freeze
 
-  ACCOUNTS = [{"created"=>0,
-      "username"=>"some_accoount.gmail.com_some_numbers",
-      "suspended"=>nil,
-      "id"=>"some_numbers",
-      "email_addresses"=>["some_accoount.gmail.com"],
-      "first_name"=>"Some",
-      "last_name"=>"Account",
-      "password_expired"=>0,
-      "sources"=>
-       [{"server"=>"imap.googlemail.com",
-         "label"=>"some_account::gmail",
-         "username"=>"some_account.com",
-         "port"=>993,
-         "authentication_type"=>"oauth2",
-         "use_ssl"=>true,
-         "status"=>"OK",
-         "type"=>"imap",
-         "resource_url"=>"https://api.context.io/2.0/accounts/"}],
-      "resource_url"=>"https://api.context.io/2.0/accounts/"},
-     {"created"=>0,
-      "username"=>"asecondaccount.gmail.com_some_numbers",
-      "suspended"=>nil,
-      "id"=>"some_other_numbers",
-      "email_addresses"=>["asecondaccount@gmail.com"],
-      "first_name"=>"Amuro",
-      "last_name"=>"Ray",
-      "password_expired"=>0,
-      "sources"=>
-       [{"server"=>"imap.googlemail.com",
-         "label"=>"asecondaccount::gmail",
-         "username"=>"qsecondaccount@gmail.com",
-         "port"=>993,
-         "authentication_type"=>"oauth2",
-         "use_ssl"=>true,
-         "status"=>"OK",
-         "type"=>"imap",
-         "resource_url"=>"https://api.context.io/2.0/accounts/"}],
-      "resource_url"=>"https://api.context.io/2.0/accounts/"}].freeze
 
-  MOCK_FARDAY_COLLECTION = Struct::MockFaraday.new(ACCOUNTS, TestingConstants::SUCCESSFUL_CALL, true)
-
-  MOCK_FARADAY = Struct::MockFaraday.new(MOCK_FARADAY_SUCCESS_BODY, TestingConstants::SUCCESSFUL_CALL, true)
 end
