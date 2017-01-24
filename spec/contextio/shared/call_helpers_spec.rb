abe6817b421b640a19122cea418c0ea4e7cc1e12
Dane Carmichael <carmichaeldane@gmail.com>
2017-01-24 09:34:56 -0600

    Condensing and matching the tests style for the call_helpers_spec

--- spec/contextio/shared/call_helpers_spec.rb
+++ spec/contextio/shared/call_helpers_spec.rb
@@ -1,14 +1,11 @@
 module ContextIO
   describe CallHelpers do
     let(:call_helper) { Class.new { extend CallHelpers } }
-    subject { call_helper.get_params([:allowed, :not_allowed],[:allowed]) }
 
-    it "rejected not_allowed parameters" do
-      subject[1] == [:not_allowed]
-    end
-
-    it "allowed correct parameters" do
-      subject[0] == [:allowed]
+    it "separates allowed and not allowed parameters" do
+      params = call_helper.get_params(%i(allowed not_allowed), %i(allowed))
+      expect(params.first).to eq Hash(allowed: nil)
+      expect(params.last).to eq [:not_allowed]
     end
   end
 end
